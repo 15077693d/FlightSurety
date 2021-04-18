e99eb56ff4c9a65d179a3e04669d3af362bb1b0e
unknown <15077693d@gmail.com>
2021-04-19 07:30:09 +0800

    add instruction

--- website/src/App.js
+++ website/src/App.js
@@ -1,6 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+/*
+Airline
+ 1. show and set operational (Operational Setup)
+ 2. show and reg airline and add ether (Airline Registration)
+ 3. show and add flight  (Flight Registration)
+ 4. Cancel flight, Insurance Repayment (Insurance Setup)
 
+Passengers
+ 1.PassengersBuy insurance (Insurance Purchase)
+ 2. Money Withdraw
+ 3. check Flight Status
+*/
 function App() {
   return (
     <div className="App">
